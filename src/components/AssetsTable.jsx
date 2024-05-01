7c64e235c59a7249c7e7fb9f127434cf14e59993
Magistry <Magistrigusti@yandex.ru>
2024-05-01 21:57:55 +0300

    0.02

---
+++ src/components/AssetsTable.jsx
@@ -0,0 +1,64 @@
+
+import { Table } from 'antd';
+import { useCrypto } from '../context/crypto-context';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    showSorterTooltip: {
+      target: 'full-header',
+    },
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ['descend'],
+  },
+  {
+    title: 'Price, $',
+    dataIndex: 'price',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+  },
+];
+
+
+export default function AssetsTable() {
+  const { assets } = useCrypto();
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+    />
+  );
+};
---
+++ src/try.js
@@ -0,0 +1,34 @@
+import { Layout, Typography } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
+
+const contentStyle = {
+  textAlign: "center",
+  minHeight: 'calc(100vh - 60px)',
+  color: "#fff";
+  backgroundColor: "#001529",
+  padding: '1rem',
+}
+
+export default function AppContent() {
+  const { asset, crypto} = useCrypto();
+
+  const cryptoPriceMap = crypto.reduce((acc, c) => {
+    acc[c.id] = c.price
+    return acc
+  }, {})
+
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3}
+        style = {{ textAlig: "left", color: "#fff"}}
+      >
+      Protfolio:{" "}
+      {asset
+        .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+        .reducer((acc, v) => (acc += v), 0)
+        .toFixed(2)
+      }$
+      </Typography.Title>
+    </Layout.Content>
+  )
+}
